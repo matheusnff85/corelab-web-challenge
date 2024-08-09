42eb79b8e6b957bf9ccbdc28b622d6f05eca5eaf
matheusnff85 <matheus.marinhodsp@gmail.com>
2024-08-09 18:12:36 -0300

    feat: finaliza component para criação de uma nova task

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../components/header";
+import { TaskCreator } from "../components/task-creator";
 
 export default function Home() {
   return (
     <main className="pb-24 flex flex-col gap-10">
       <Header></Header>
+      <TaskCreator></TaskCreator>
     </main>
   );
 }
